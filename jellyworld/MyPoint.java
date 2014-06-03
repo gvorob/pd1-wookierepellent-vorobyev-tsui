c640b8aaa69313d09b423f19baef25d955470faf
DT <dtsui918@yahoo.com>
2014-06-03 08:39:20 -0400

    point update - mass,velocity

--- jellyworld/MyPoint.java
+++ jellyworld/MyPoint.java
@@ -1,25 +1,47 @@
 package jellyworld;
 import java.awt.*;
+import java.util.*;
 
 public class MyPoint implements Drawable, Entity{
-	private Vector2 pos;
-	
-	public MyPoint(Vector2 p){
-		pos = p.clone();
-		World.addEntity(this);
-		World.addDrawable(this);
-	}
-	
-	public void update(float time){//@override Entity
-		
-	}
+    private Vector2 pos;
+    private Vector2 velocity;
+    private Vector2 gravity;
+    private double mass;
+    private int pointSize;
 
-	public void draw(Graphics g){//@override Drawable
-		g.setColor(Color.red);
-		g.fillOval((int)(pos.x - 5),(int)(pos.y - 5), 10, 10);
-	}
+    private LinkedList<MyPoint> neighbors;
+	
+    public MyPoint(Vector2 p){
+	pos = p.clone();
+	World.addEntity(this);
+	World.addDrawable(this);
+	pointSize = 10;
 
-	public boolean isDead(){return false;}//@override Entity
-	public boolean isErased(){return false;}//@override Drawable
-	public int getZOrder(){return 0;}//for now
+	gravity = new Vector2(0,1);
+	velocity = new Vector2(0,1);
+
+	mass = 10;
+    }
+	
+    public void update(float time){//@override Entity
+	// update for gravity
+	velocity.add(gravity);
+	pos.add(velocity);
+	// update for wall bounce
+	if (pos.y >= JellyWorld.s.screen.c.getHeight() - (pointSize / 2)){
+	    velocity.y *= -0.9;
+	    pos.y = JellyWorld.s.screen.c.getHeight() - (pointSize / 2);
+	}
+    }
+
+    public void draw(Graphics g){//@override Drawable
+	g.setColor(Color.red);
+	g.fillOval((int)(pos.x - (pointSize / 2)),(int)(pos.y - (pointSize / 2)), pointSize, pointSize);
+    }
+
+    public boolean isDead(){return false;}//@override Entity
+    public boolean isErased(){return false;}//@override Drawable
+    public int getZOrder(){return 0;}//for now
+    
+    
 }
