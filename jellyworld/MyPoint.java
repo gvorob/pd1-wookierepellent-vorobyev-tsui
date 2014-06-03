39d91935ac750257c49cc4e8ac3841f69263a999
George Vorobyev <quaffle97@gmail.com>
2014-06-03 08:39:48 -0400

    Added getNodesWithin

--- jellyworld/MyPoint.java
+++ jellyworld/MyPoint.java
@@ -1,13 +1,18 @@
 package jellyworld;
 import java.awt.*;
+import java.util.*;
 
 public class MyPoint implements Drawable, Entity{
+
+	public static LinkedList<MyPoint> Nodes = new LinkedList<MyPoint>();
+
 	private Vector2 pos;
 	
 	public MyPoint(Vector2 p){
 		pos = p.clone();
 		World.addEntity(this);
 		World.addDrawable(this);
+		Nodes.add(this);
 	}
 	
 	public void update(float time){//@override Entity
@@ -19,7 +24,20 @@ public class MyPoint implements Drawable, Entity{
 		g.fillOval((int)(pos.x - 5),(int)(pos.y - 5), 10, 10);
 	}
 
+	public Vector2 getPos(){
+		return pos.clone();
+	}
+
 	public boolean isDead(){return false;}//@override Entity
 	public boolean isErased(){return false;}//@override Drawable
 	public int getZOrder(){return 0;}//for now
+
+	public LinkedList<MyPoint> getNodesWithin(double dist, Vector2 pos){
+		LinkedList<MyPoint> temp = new LinkedList();
+		for(MyPoint n : Nodes){
+			if(n.getPos().distTo(pos) <= dist)
+				temp.add(n);
+		}
+		return n;
+	}
 }
