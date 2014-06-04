22617df65d2b5c36540a510dca9bc7a9d59476bc
DT <dtsui918@yahoo.com>
2014-06-04 08:30:34 -0400

    mypoint links

--- Log.md
+++ Log.md
@@ -1,3 +1,5 @@
 Log
-6/3/14		Derek		MyPoint has mass, velocity, gravity
-6/3		George		MyPoint has a static list of all points and a static method to get nearby points
+6/03	Derek	MyPoint has mass, velocity, gravity
+6/03	George	MyPoint has a static list of all points and a static method to get nearby points
+6/04	Derek
+6/04	George	Link class for springs between points
--- jellyworld/MyPoint.java
+++ jellyworld/MyPoint.java
@@ -9,16 +9,23 @@ public class MyPoint implements Drawable, Entity{
     private double mass;
     private int pointSize;
 
-    private LinkedList<MyPoint> neighbors;
-	public static LinkedList<MyPoint> Nodes = new LinkedList<MyPoint>();
+    private LinkedList<Link> neighbors;
+    public static LinkedList<MyPoint> Nodes = new LinkedList<MyPoint>();
 	
     public MyPoint(Vector2 p){
 	pos = p.clone();
+	pointSize = 10;
+	neighbors = new LinkedList<Link>();
+	
+	LinkedList<MyPoint> tempNeighbors = new LinkedList<MyPoint>(getNodesWithin(pointSize * 5, this.pos));
+	for (MyPoint q : tempNeighbors){
+	    neighbors.add(new Link(0.5, this.pos.distTo(q.getPos()), this));
+	}
+
 	Nodes.add(this);
 	World.addEntity(this);
 	World.addDrawable(this);
-	pointSize = 10;
-
+	
 	gravity = new Vector2(0,1);
 	velocity = new Vector2(0,1);
 
@@ -41,20 +48,20 @@ public class MyPoint implements Drawable, Entity{
 	g.fillOval((int)(pos.x - (pointSize / 2)),(int)(pos.y - (pointSize / 2)), pointSize, pointSize);
     }
 
-	public Vector2 getPos(){
-		return pos.clone();
-	}
+    public Vector2 getPos(){
+	return pos.clone();
+    }
 
-	public boolean isDead(){return false;}//@override Entity
-	public boolean isErased(){return false;}//@override Drawable
-	public int getZOrder(){return 0;}//for now
+    public boolean isDead(){return false;}//@override Entity
+    public boolean isErased(){return false;}//@override Drawable
+    public int getZOrder(){return 0;}//for now
 
-	public LinkedList<MyPoint> getNodesWithin(double dist, Vector2 pos){
-		LinkedList<MyPoint> temp = new LinkedList<MyPoint>();
-		for(MyPoint n : Nodes){
-			if(n.getPos().distTo(pos) <= dist)
-				temp.add(n);
-		}
-		return temp;
+    public static LinkedList<MyPoint> getNodesWithin(double dist, Vector2 pos){
+	LinkedList<MyPoint> temp = new LinkedList<MyPoint>();
+	for(MyPoint n : Nodes){
+	    if(n.getPos().distTo(pos) <= dist)
+		temp.add(n);
 	}
+	return temp;
+    }
 }
