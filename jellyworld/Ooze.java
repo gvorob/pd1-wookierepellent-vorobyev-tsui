5bf3d64098874fcf50c9edd9a3e71d7cbed17292
d-tsui <dtsui918@yahoo.com>
2014-06-15 12:34:38 -0400

    links reform within connectRange

--- jellyworld/Ooze.java
+++ jellyworld/Ooze.java
@@ -3,9 +3,11 @@ import java.awt.*;
 import java.util.*;
 
 public class Ooze extends MyPoint{
-
+	private double connectRange;
+	
     public Ooze (Vector2 p, boolean fixed, double connectRange){
 	super(p, fixed,connectRange);
+	this.connectRange = connectRange;
     }
 
     public void update1(float time){//@override Entity
@@ -14,13 +16,24 @@ public class Ooze extends MyPoint{
 	for (Link l : neighbors){
 	    tempForce = Vector2.vecSubt(l.other.getPos(), this.pos);
 	    tempForce.setLength((float)(l.k * (tempForce.length() - l.len)));
-	    if (tempForce.length() > 150){
+	    if (tempForce.length() > connectRange){
 		temp.add(l.other);
 	    }
 	}
 	for (MyPoint p : temp){
 	    p.removeNeighbor(this);
 	    this.removeNeighbor(p);
+	}
+	
+	Link temp2;
+	boolean existing = false;
+	for (MyPoint p : MyPoint.getNodesWithin(connectRange * 0.75,this.getPos())){
+		temp2 = new Link(constant, this.pos.distTo(p.getPos()), p);
+		existing = false;
+		for (Link l : neighbors){
+			if(l.equals(temp2)) existing = true;
+		}	
+		if (!existing) addLink(p);
 	}
 	super.update1(time);
     }
